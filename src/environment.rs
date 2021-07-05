356cb237fdbdca768f9b254b4169b315e6c8eb81
fiveseven-lambda <fiveseven.lambda@gmail.com>
2021-07-05 21:36:02 +0900

    true / false を追加

--- src/environment.rs
+++ src/environment.rs
@@ -3,15 +3,25 @@
 use crate::{compiler, error, syntax, value};
 use std::collections::HashMap;
 
+use std::cell::Cell;
+use std::rc::Rc;
+
 pub struct Environment {
     variables: HashMap<String, value::Value>,
 }
 
 impl Environment {
     pub fn new() -> Environment {
-        Environment {
-            variables: HashMap::new(),
-        }
+        let mut variables = HashMap::new();
+        variables.insert(
+            "true".to_string(),
+            value::Value::Boolean(Rc::new(Cell::new(true))),
+        );
+        variables.insert(
+            "false".to_string(),
+            value::Value::Boolean(Rc::new(Cell::new(false))),
+        );
+        Environment { variables }
     }
     pub fn run(&mut self, statement: syntax::Statement) -> Result<(), error::Error> {
         compiler::compile_statement(statement, &mut self.variables)?.run();
